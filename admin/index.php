1034a185a59aa13402ae01cd3989030aba96da19
josh055 <adejoh382@gmail.com>
2020-09-07 14:03:30 +0100

    first commit of proect to git

---
+++ admin/index.php
@@ -0,0 +1,39 @@
+
+<?php include'includes/Admin_header.php'?>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+       <?php include'includes/Admin_navigation.php'?>
+
+
+
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row"> 
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                           wellcome to admin
+                            
+                            <small><?php echo $_SESSION['username'];?></small>
+                        </h1>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+
+        
+
+
+        <!-- /#page-wrapper -->
+<?php include'includes/Admin_footer.php'?>
+    
